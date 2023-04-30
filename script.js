f58606a86a27d31231a0bbc65e63d678f43edfea
PhavadaraneeB <132197240+PhavadaraneeB@users.noreply.github.com>
2023-04-30 19:02:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function runSpeechRecognition() {
+    // get output div reference
+    var output = document.getElementById("output");
+    // get action element reference
+    var action = document.getElementById("action");
+    // new speech recognition object
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
+
+    // This runs when the speech recognition service starts
+    recognition.onstart = function() {
+        output.innerHTML = "<br><br><br>";
+        action.innerHTML = `<br><br><div class="loading text-center">
+        <svg width="64px" height="48px">
+            <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="back"></polyline>
+          <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="front"></polyline>
+        </svg>
+      </div>`;
+    };
+    
+    recognition.onspeechend = function() {
+        action.innerHTML = "";
+        recognition.stop();
+    }
+  
+    // This runs when the speech recognition service returns result
+    recognition.onresult = function(event) {
+        var transcript = event.results[0][0].transcript;
+        var confidence = event.results[0][0].confidence;
+        output.innerHTML = "<br>"+transcript+"<br><br>";
+        output.classList.remove("hide");
+    };
+  
+     // start recognition
+     recognition.start();
+}
